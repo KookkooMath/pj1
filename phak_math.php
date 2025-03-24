0f8721108c5f83621c622009cb11507b8440e987
KookkooMath <64050347@kmitl.ac.th>
2025-03-24 16:44:43 +0700

    Increase header height and adjust margins in Login and Register pages for improved layout

--- phak_math.php
+++ phak_math.php
@@ -6,7 +6,7 @@ $password = "";
 $dbname = "phak_math";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3307);
 $conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
